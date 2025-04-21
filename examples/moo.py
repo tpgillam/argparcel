74496f4504f4dd9e0b660c7b03b5736462f6081a
Tom Gillam <tpgillam@googlemail.com>
2025-04-21 19:48:10 +0100

    support literals

--- examples/moo.py
+++ examples/moo.py
@@ -1,5 +1,7 @@
+import argparse
 import dataclasses
 import pathlib
+from typing import Literal
 import rich
 
 import argparcel
@@ -9,13 +11,14 @@ import argparcel
 class Moo:
     a: int | None
     b: float
+    choice: Literal[1, 2, 3] | None
     path: pathlib.Path | None
     c: bool = True
     description: str | None = None
 
 
-rich.print(argparcel.parse(Moo, ["--a", "2", "--b", "3.2"]))
-rich.print(argparcel.parse(Moo, ["--a", "2", "--b", "3.2", "--no-c"]))
+rich.print(argparcel.parse(Moo, ["--a", "2", "--b", "3.2", "--choice", "1"]))
+rich.print(argparcel.parse(Moo, ["--a", "2", "--b", "3.2", "--no-c", "--choice", "3"]))
 rich.print(argparcel.parse(Moo, ["--b", "4", "--c"]))
 rich.print(argparcel.parse(Moo, ["--b", "4", "--c", "--description", "moo moo"]))
 rich.print(
@@ -32,5 +35,22 @@ rich.print(
         ],
     )
 )
+
 print()
-rich.print(argparcel.parse(Moo, ["--help"]))
+print()
+
+
+@dataclasses.dataclass(kw_only=True, frozen=True, slots=True)
+class Moo2:
+    choice: Literal[1, 2, 3] | None
+    no_choice: Literal["foo", "bar"]
+
+
+rich.print(argparcel.parse(Moo2, ["--choice", "2", "--no-choice", "bar"]))
+rich.print(argparcel.parse(Moo2, ["--no-choice", "foo"]))
+try:
+    rich.print(argparcel.parse(Moo2, [], exit_on_error=False))
+except argparse.ArgumentError:
+    pass
+
+
--- src/argparcel/__init__.py
+++ src/argparcel/__init__.py
@@ -28,19 +28,26 @@ def parse[T: _typeshed.DataclassInstance](
 
         no_default = field.default is dataclasses.MISSING
 
-        if type(field.type) not in (type, types.UnionType):
-            # NOTE: we do not yet support `types.GenericAlias` instances, e.g.
-            #   `list[int]`
+        if type(field.type) not in (
+            type,
+            types.UnionType,
+            typing._UnionGenericAlias,  # pyright: ignore [reportAttributeAccessIssue]
+            typing._LiteralGenericAlias,  # pyright: ignore [reportAttributeAccessIssue]
+        ):
             _fail(field)
-        field_type = typing.cast("type | types.UnionType", field.type)
+        field_type = typing.cast(
+            "type | types.UnionType | typing._UnionGenericAlias | typing._LiteralGenericAlias",  # pyright: ignore [reportAttributeAccessIssue]
+            field.type,
+        )
 
         # If the type includes `None`, then we allow the type to
-        if isinstance(field_type, types.UnionType):
+        if isinstance(
+            field_type,
+            types.UnionType | typing._UnionGenericAlias,  # pyright: ignore [reportAttributeAccessIssue]
+        ):
             base_types = typing.get_args(field_type)
-            allow_missing = any(issubclass(x, types.NoneType) for x in base_types)
-            non_none_types = tuple(
-                x for x in base_types if not issubclass(x, types.NoneType)
-            )
+            allow_missing = any(x is types.NoneType for x in base_types)
+            non_none_types = tuple(x for x in base_types if x is not types.NoneType)
             if len(non_none_types) != 1:
                 _fail(field)
             base_type = non_none_types[0]
@@ -66,8 +73,25 @@ def parse[T: _typeshed.DataclassInstance](
         kwargs["required"] = no_default and (not allow_missing)
 
         if base_type is bool:
+            # Represent boolean arguments as 'flags'
             parser.add_argument(name, action=argparse.BooleanOptionalAction, **kwargs)
+        elif isinstance(base_type, typing._LiteralGenericAlias):  # pyright: ignore [reportAttributeAccessIssue]
+            # Represent literal arguments with choices.
+            # We enforce that they MUST all be of the same type, so that we can convert
+            # them simply and unambiguously (e.g. `Literal[42, "42"]` could cause
+            # problems).
+            choices = typing.get_args(base_type)
+            choice_types = {type(x) for x in choices}
+            if len(choice_types) != 1:
+                msg = (
+                    "Need exactly one type of choice. "
+                    f"Found {choice_types} for {field_type}, for field '{field.name}'"
+                )
+                raise ValueError(msg)
+            (type_,) = choice_types
+            parser.add_argument(name, type=type_, choices=choices, **kwargs)
+
         else:
-            parser.add_argument(name, type=base_type, **kwargs)
+            parser.add_argument(name, type=base_type, **kwargs)  # pyright: ignore [reportArgumentType]
 
     return cls(**parser.parse_args(command_line).__dict__)
